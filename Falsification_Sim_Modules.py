cbebfe285086602aa884e0b268d124bd45a37659
eugenewickett <eugenewickett@gmail.com>
2020-03-26 13:31:34 -0500

    Add files via upload

---
+++ Falsification_Sim_Modules.py
@@ -0,0 +1,250 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 14 17:04:36 2019
+
+@author: Eugene Wickett
+
+Stores modules for use with 'SC Simulator.py'
+"""
+
+import numpy as np
+import random
+import csv
+
+def generateNodeListsFromFile(nodeInputFileString='',
+                              arcPreferencesFileString='',
+                              arcLTsFileString='',
+                              arcRsFileString='',
+                              NumSimDays=0,     
+                              ):
+    """
+    Processes node and arc files and returns node lists 
+    """
+    List_RootNode = []
+    List_IntermediateNode = []
+    List_EndNode = []
+    # Read the arc and node files as matrices/lists
+    with open(nodeInputFileString) as nodeFile, open(arcPreferencesFileString) as arcPrefsFile, open(arcLTsFileString) as arcLTsFile, open(arcRsFileString) as arcRsFile:
+        nodeReader = csv.reader(nodeFile,delimiter=',')
+        nodeList = list(nodeReader)
+        arcPreferencesReader = csv.reader(arcPrefsFile,delimiter=',')
+        arcPrefsList = list(arcPreferencesReader)
+        arcPreferencesMatrix = np.array(arcPrefsList).astype("float") #Preferences in matrix form
+        arcLTsReader = csv.reader(arcLTsFile,delimiter=',')
+        arcLTsList = list(arcLTsReader)
+        arcLTsMatrix = np.array(arcLTsList).astype("float") #LTs in matrix form
+        arcRsReader = csv.reader(arcRsFile,delimiter=',')
+        arcRsList = list(arcRsReader)
+        arcRsMatrix = np.array(arcRsList).astype("float") #LTs in matrix form
+        
+        
+    # Generate objects, lists, etc. for the nodes
+    nodeListHeader = nodeList[0] # Remove the headers from the list
+    nodeList = nodeList[1:] # Retain the node rows only
+    nodeNum = len(nodeList)
+    for currNode in range((len(nodeList))):
+        currRootTF = nodeList[currNode][0]            # Root node?
+        currEndTF = nodeList[currNode][1]             # End node?
+        if currRootTF == 'FALSE':
+            currReorderPoint = int(nodeList[currNode][2])
+            currReorderAmount = int(nodeList[currNode][3])
+        if currEndTF == 'TRUE': # We have an end node
+            currDemandDistribution = int(nodeList[currNode][4])
+            currDemandParamList = []
+            if not nodeList[currNode][5] == '':
+                currDemandParam1 = float(nodeList[currNode][5])
+                currDemandParamList.append(currDemandParam1)
+            if not nodeList[currNode][6] == '':
+                currDemandParam2 = float(nodeList[currNode][6])
+                currDemandParamList.append(currDemandParam2)
+            if not nodeList[currNode][7] == '':
+                currDemandParam3 = float(nodeList[currNode][7])
+                currDemandParamList.append(currDemandParam3)
+        
+        # Add root node to List_RootNode; otherwise generate the preferred supplier list for this non-root node
+        if currRootTF == 'TRUE': # Currently a root node
+            List_RootNode.append(currNode) # List_Root only has integers
+        
+        else: # Generate preferred supplier list
+            currPreferredSuppliers = [] # Initialize our preferred supplier list
+            currPreferredSuppliersLTs = []
+            currPreferredSuppliersRs = []
+            numSuppliers = int(max(arcPreferencesMatrix.T[currNode])) # How many suppliers are ranked
+            for indSupplier in range(numSuppliers): # Add these suppliers by rank
+                findNum = indSupplier+1
+                for currSupplier in range(nodeNum): # Find the next ranked supplier
+                    if arcPreferencesMatrix.T[currNode][currSupplier] == findNum:
+                        currPreferredSuppliers.append(currSupplier) #Add the supplier's node number
+                        currPreferredSuppliersLTs.append(int(arcLTsMatrix[currSupplier][currNode])) # Add the lead time from the supplier to the current Node
+                        currPreferredSuppliersRs.append(int(arcRsMatrix[currSupplier][currNode])) # Add the lead time from the supplier to the current Node
+            # Supplier lists are now set            
+            
+            # Generate Node objects, including demand schedules if the current node is a root node
+            if currEndTF == 'FALSE': # Currently an intermediate node
+                # Add a node object to the List_IntermediateNode
+                newIntermediateNode = Node(nodeNum=currNode, reorderPoint=currReorderPoint, reorderAmount=currReorderAmount, preferredSupplierVec=currPreferredSuppliers, preferredSupplierLTsVec=currPreferredSuppliersLTs, preferredSupplierRsVec=currPreferredSuppliersRs, endNodeTF = False)
+                List_IntermediateNode.append(newIntermediateNode)
+            else: # Currently an end node
+                # Add a node object to the List_End
+                newDemandSchedule = demandScheduleGenerator_EndNode(int_numDays=NumSimDays, int_DistType=currDemandDistribution, arr_DistParameter=currDemandParamList)
+                newEndNode = Node(nodeNum=currNode, reorderPoint=currReorderPoint, reorderAmount=currReorderAmount, preferredSupplierVec=currPreferredSuppliers, preferredSupplierLTsVec=currPreferredSuppliersLTs, preferredSupplierRsVec=currPreferredSuppliersRs, endNodeTF=True,demandSched=newDemandSchedule)
+                List_EndNode.append(newEndNode)    
+    ### END NODE FOR LOOP ###
+    return List_RootNode, List_IntermediateNode, List_EndNode, nodeListHeader, nodeList, nodeNum, arcPreferencesMatrix, arcLTsMatrix, arcRsMatrix
+
+
+def demandScheduleGenerator_EndNode(int_numDays=1000,
+                             int_DistType = 0, 
+                             arr_DistParameter = [4]
+                             ):
+    """
+    Randomly generates a demand schedule for a desired number of time units, 
+    with the following parameters entered:
+            1) int_numDays: Number of batches desired
+            2) int_DistType: Distribution for the demand of each time period
+                0 = Constant; parameter array should be [Constant value]
+                1 = Uniform; paramter array should be [Low Bound, High Bound]
+                2 = Triangular; paramter array should be [Left,Mode,Right]
+                ...
+            3) arr_DistParameter: Array of the parameters required for generatng
+                the demands
+                0 = Constant; parameter array should be [Constant value]
+                1 = Uniform; paramter array should be [Low Bound, High Bound]
+                2 = Triangular; parameter array should be [Left,Mode,Right]
+                ...
+    
+    Outputs a Python list with the following elements within each entry:
+            1) Demand: Integer denoting the demand for each simulation day
+    """
+    #Initialize our output, a list with the above mentioned outputs
+    demandSchedule = []
+    
+    if int_DistType == 0: #Constant
+        #Use the first value of the parameter array
+        demandSchedule = np.repeat(arr_DistParameter[0],int_numDays)
+    elif int_DistType == 1: #Uniform 
+        #Use the first two values of the parameter array
+        demandSchedule = np.round(np.random.uniform(low=arr_DistParameter[0],high=arr_DistParameter[1],size=int_numDays))      
+    elif int_DistType == 2: #Triangular
+        #Use the first two values of the parameter array
+        demandSchedule = np.round(np.random.triangular(left=arr_DistParameter[0],mode=arr_DistParameter[1],right=arr_DistParameter[2],size=int_numDays))
+    else:
+        print('Error generating the demand schedule.')
+    
+    return demandSchedule
+
+ ### END "demandScheduleGenerator_EndNode" ###       
+
+def testingScheduleGenerator(nodes = [], int_numDays=1000,
+                             int_sampleBudget = 1000,
+                             int_PolicyType = 0, 
+                             arr_PolicyParameter = [0]
+                             ):
+    """
+    Generates a testing schedule list for a desired number of time units, 
+    with the following parameters entered:
+            0) nodes: The node list generated from the generateNodeListsFromFile function
+            1) int_numDays: Number of schedule days desired
+            2) int_sampleBudget: Budget amount, in number of samples
+            3) int_PolicyType: Desired policy type, one of the following
+                0 = Deterministic-End Node; samples 1 end node each day
+                ...
+            4) arr_PolicyParameter: Array of the parameters required for generatng
+                the test schedules
+                ...
+    
+    Outputs a Python list with the following elements within each entry:
+            1) Day: Simulation day of the test
+            2) Node: Which node to test that day
+    """
+    #Initialize our output, a list with the above mentioned outputs
+    sampleSchedule = []
+    
+    if int_PolicyType == 0: # Deterministic End-Node policy
+        # Identify the list of end nodes
+        endNodes = []
+        for nodeInd in range(len(nodes)):
+            if nodes[nodeInd][1] == 'TRUE': #We have an end node
+                endNodes.append(nodeInd)
+        # Generate a sampling schedule iterating through each end node
+        nodeCount = 0
+        currNode = endNodes[nodeCount]
+        lastEndNode = endNodes[-1]            
+        for i in range(int_numDays): 
+            sampleSchedule.append([i,currNode])
+            if currNode == lastEndNode:
+                nodeCount = 0
+                currNode = endNodes[nodeCount]
+            else:
+                nodeCount += 1
+                currNode = endNodes[nodeCount]
+        ### END DETERMINISTIC END-NODE POLICY
+    
+    elif int_PolicyType == 1: # Deterministic End-Node policy with multiple tests per day, set via int_sampleBudget
+        # Identify the list of end nodes
+        endNodes = []
+        for nodeInd in range(len(nodes)):
+            if nodes[nodeInd][1] == 'TRUE': #We have an end node
+                endNodes.append(nodeInd)
+        # Generate a sampling schedule iterating through each end node
+        nodeCount = 0
+        currNode = endNodes[nodeCount]
+        lastEndNode = endNodes[-1]        
+        for i in range(int_sampleBudget):
+            day = np.mod(i,int_numDays)
+            sampleSchedule.append([day,currNode])
+            if currNode == lastEndNode:
+                nodeCount = 0
+                currNode = endNodes[nodeCount]
+            else:
+                nodeCount += 1
+                currNode = endNodes[nodeCount]
+        ### END DETERMINISTIC END-NODE POLICY WITH MULTIPLE TESTS
+    
+    elif int_PolicyType == 2: # Randomized End-Node policy with multiple tests per day, set via int_sampleBudget
+        # Identify the list of end nodes
+        endNodes = []
+        for nodeInd in range(len(nodes)):
+            if nodes[nodeInd][1] == 'TRUE': #We have an end node
+                endNodes.append(nodeInd)
+        numEndNodes = len(endNodes)
+        
+        # Generate a sampling schedule randomly sampling the list of end nodes
+        for i in range(int_sampleBudget):
+            day = np.mod(i,int_numDays)
+            currEndInd = int(np.floor(np.random.uniform(low=0,high=numEndNodes,size=1)))
+            currNode = endNodes[currEndInd]
+            sampleSchedule.append([day,currNode])
+        ### END RANODMIZED END-NODE POLICY WITH MULTIPLE TESTS
+    
+    else:
+        print('Error generating the sampling schedule.')
+    
+    # Need to sort this list before passing it through
+    sampleSchedule.sort(key=lambda x: x[0])
+    return sampleSchedule
+
+ ### END "testingScheduleGenerator" ###
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
