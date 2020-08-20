96f91d6fd95cdcaa138a91523e459ed301576e1b
Eugene <eugenewickett@gmail.com>
2020-08-20 11:29:49 -0500

    debug 6

--- Maya/questDebug2.py
+++ Maya/questDebug2.py
@@ -5,7 +5,7 @@ Created on Wed Aug 19 16:49:53 2020
 @author: eugen
 """
 import numpy as np
-import scipy.optimize as spo
+import scipy 
 
 def PlumleeEstimates(ydata, numsamples, A, sens, spec, rglrWt = 0.1):
     ydata = np.array(ydata)
@@ -47,13 +47,15 @@ nsamp = [100,100,100,100,100,100]
 #print(iProj)
 #print(eProj)
 
+'''
 def sqrdFunc(x):
     return x**2
 bds = spo.Bounds(np.array([1]),np.array([1e3]))
 x0 = np.array([2])
 opval = spo.minimize(sqrdFunc,x0,options={'disp':False},bounds=bds)
 print(opval.x)
+'''
 
-
+print(scipy.__version__)
 
 
